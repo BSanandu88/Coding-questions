aab92fe6e84d695e0f965c05f8d19260b84712c8
B S Anandu <59107214+BSanandu88@users.noreply.github.com>
2020-08-12 22:01:31 +0530

    codeforces round #662

---
+++ A. Rainbow Dash, Fluttershy and Chess Coloring.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+         int t;cin>>t;
+         long long int n,n1;
+         while(t--){
+                  cin>>n;
+                  n1 = n/2;
+                  cout<<n1+1<<endl;
+         }
+         return 0;
+}
