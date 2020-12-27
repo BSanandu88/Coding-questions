bcb9a42036f2af98bba82418a71fd94627a89e66
BSanandu88 <bsanandu88@gmail.com>
2020-12-27 15:21:04 +0530

    remove duplicate

---
+++ removeDuplicates.cpp
@@ -0,0 +1,23 @@
+//remove duplicates from the given string
+#include<iostream>
+
+using namespace std;
+
+
+string removeDup(string s){
+    if(s.length() == 0){
+        return "";
+    }
+    char ch = s[0];
+    string ans = removeDup(s.substr(1));
+    if(ch == ans[0]){
+        return ans;
+    }
+    return (ch + ans);
+}
+
+
+int main(){
+    cout<<removeDup("aaaabbcceddd")<<endl;
+    return 0;
+}
