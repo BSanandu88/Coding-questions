aab276b5f76dca12f5a9551473e81e691c0713ce
BSanandu88 <bsanandu88@gmail.com>
2020-12-28 17:19:25 +0530

    distinct integers

---
+++ permutation/distinctIntegers.cpp
@@ -0,0 +1,34 @@
+//Given an array nums of distinct integers return all the possible permutations .You can return the answer in any order
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<vector<int>>ans;
+
+void permute(vector<int> &a,int idx){
+    if(idx==a.size()){
+        ans.push_back(a);
+        return;
+    }
+    for(int i=idx;i<a.size();i++){
+        swap(a[i],a[idx]);
+        permute(a,idx+1);
+        swap(a[i],a[idx]);
+    }
+  return;
+}
+int32_t main(){
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(auto &i : a){
+        cin>>i;
+    }
+    permute(a,0);
+    for(auto v : ans){
+           for(auto i : v ){
+               cout<<i<<" ";
+           }
+               cout<<"\n";
+    }
+    return 0;
+}
