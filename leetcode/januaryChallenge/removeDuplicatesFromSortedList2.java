c2ee372e0a3f0d7f7a56c2304099035dd8e013d4
BSanandu88 <bsanandu88@gmail.com>
2021-01-06 07:07:09 +0530

    remove duplicates

---
+++ leetcode/januaryChallenge/removeDuplicatesFromSortedList2.java
@@ -0,0 +1,33 @@
+//Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = new ListNode(0,head);
+        ListNode prev = temp;
+        
+        while(head != null){
+            if(head.next != null && head.val == head.next.val){
+                //remove the node which are equal
+                while(head.next != null && head.val == head.next.val){
+                    head = head.next;
+                }
+                prev.next = head.next;
+            }
+            else{
+                prev = prev.next;
+            }
+            head = head.next;
+        }
+        return temp.next;
+    }
+}
