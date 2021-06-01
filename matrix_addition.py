6ebfec9e6de333729cf245ad13c05b590045cb9d
BSanandu88 <bsanandu88@gmail.com>
2021-06-01 16:18:22 +0530

    matrix addition

---
+++ matrix_addition.py
@@ -0,0 +1,26 @@
+r1 = [12,3,23]
+r2 = [21,5,6]
+r3 = [6,7,8]
+
+s1 = [1,2,3]
+s2 = [45,6,54]
+s3 = [23,90,67]
+
+a = []
+a.append(r1)
+a.append(r2)
+a.append(r3)
+
+b=[]
+b.append(s1)
+b.append(s2)
+b.append(s3)
+
+print(a)
+print(b)
+
+c=[[0,0,0],[0,0,0],[0,0,0]]
+for i in range(3):
+    for j in range(3):
+        c[i][j] = a[i][j] + b[i][j]
+print(c)
