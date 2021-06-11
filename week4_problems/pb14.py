16594cbb9d5a1056c399bf52544f7e9aebdbd7c6
BSanandu88 <bsanandu88@gmail.com>
2021-06-11 17:46:32 +0530

    turtle

---
+++ week4_problems/pb14.py
@@ -0,0 +1,17 @@
+""" Accept a value of and animate a regular polygon of sides using Turtle. A regular polygon of 
+sides is also called an -gon. You need a Python (with Turtle) repl to code this. This can be
+selected from the drop down menu while choosing the language of your repl. You may have to go
+
+"""
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+t.color('blue')
+
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
