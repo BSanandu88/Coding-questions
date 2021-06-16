783440f6f5e780af02fef54d6e8caf33529bd649
BSanandu88 <bsanandu88@gmail.com>
2021-06-16 10:50:05 +0530

    reverse

---
+++ codeforces/CP HUBE/rev.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    long long  a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];   
+    }
+    for(int i=n-1;i>=0;i--){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
