b3828e6099b4852d7284bdabc305f6f01b97909b
BSanandu88 <bsanandu88@gmail.com>
2021-07-28 15:10:30 +0530

    gpa 1

---
+++ week_9/gpa1.py
@@ -0,0 +1,24 @@
+#problem 1
+class Point:
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y
+
+    def move(self,dx,dy):
+        self.x += dx
+        self.y += dy
+    
+    def value(self):
+        return (self.x ,self.y)
+    
+    def duplicate(self):
+        return Point(self.x ,self.y)
+
+pt1 = Point()
+pt2 = pt1.duplicate()
+inp1 = input().strip().split(',')
+inp2 = input().strip().split(',')
+pt1.move(int(inp1[0]),int(inp1[1]))
+pt2.move(int(inp2[0]),int(inp2[1]))
+print(f'Point 1: {pt1.value()}')
+print(f'Point 2: {pt2.value()}')
