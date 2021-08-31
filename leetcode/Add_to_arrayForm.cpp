c982a4a5176b1a2e818b0e931f643ad8368a719a
BSanandu88 <bsanandu88@gmail.com>
2021-08-31 16:24:51 +0530

    update

--- leetcode/Add_to_arrayForm.cpp
+++ leetcode/Add_to_arrayForm.cpp
@@ -2,7 +2,7 @@ class Solution{
     public:
         vector<int>addToArrayForm(vector<int> &num,int k){
             reverse(begin(num),end(num));
-            for(int i=0;i<k; i++){
+            for(int i=0;k; i++){
                 if(i == size(num))num.push_back(0);
                 num[i] += k;
                 k = num[i] / 10;
