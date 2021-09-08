83b3b2f84ed57590b190646f6e07775d4b9ac698
BSanandu88 <bsanandu88@gmail.com>
2021-09-08 14:43:03 +0530

    odd one

---
+++ Additional/pdsa_week1/odd_one.py
@@ -0,0 +1,27 @@
+import math
+
+def odd_one(l):
+    ans = ""
+    for i in range(len(l)):
+        for j in range(i+1,len(l)):
+            if(type(l[j]) == type(l[i])):
+                continue
+            else:
+                if(type(l[j]) == int):
+                    ans = "ans"
+                elif(type(l[j]) == str):
+                    ans = "str"
+                elif(type(l[j]) == bool):
+                    ans = "bool"
+                elif(type(l[j]) == float):
+                    ans = "int"
+                else:
+                    pass
+                break
+
+    return ans
+
+
+
+l = [1,2,3.4,5,10]
+print(odd_one(l))
