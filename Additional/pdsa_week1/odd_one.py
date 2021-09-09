7239795431ec2fcf9dd29a72c16dacaa32fead68
BSanandu88 <bsanandu88@gmail.com>
2021-09-09 18:23:56 +0530

    odd_one

--- Additional/pdsa_week1/odd_one.py
+++ Additional/pdsa_week1/odd_one.py
@@ -20,7 +20,22 @@ def odd_one(l):
         break
     return ans
 
-
+# another method
+def odd_one(L):
+    typeD = {'int' : [],'float':[],'str':[],'bool':[]}
+    for i in L:
+        if type(i) == int and len(typeD['int']) <= 1:
+            typeD['int'] += [i]
+        elif type(i) == float and len(typeD['float']) <= 1:
+            typeD['float'] += [i]
+        elif type(i) == str and len(typeD['str']) <= 1:
+            typeD['str'] += [i]
+        elif type(i) == bool and len(typeD['bool']) <= 1:
+            typeD['bool'] += [i]
+    for i,j in typeD.items():
+        if len(j) == 1:
+            return i
+        
 
 l = [1,2,False,5,10]
 print(odd_one(l))
