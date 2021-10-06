3df207e55c17eb7619eb1892d8b244d5f4a3783b
BSanandu88 <bsanandu88@gmail.com>
2021-10-06 17:21:37 +0530

    love song

---
+++ codeforces/love_song.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,q;
+    cin >> n >> q;
+    string s;
+    cin >> s;
+    long long int  v[n + 1];
+    v[0] = 0;
+    for(int i=1; i<(n + 1); i++){
+        v[i] = v[i - 1] + ((s[i - 1] - 'a') + 1);
+    }
+    while(q--){
+        int l,r;
+        cin >> l >> r;
+        cout << v[r] - v[l - 1] << endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
