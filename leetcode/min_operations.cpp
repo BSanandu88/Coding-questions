40164ab4993f5c859d8a29a173e1933334abffde
BSanandu88 <bsanandu88@gmail.com>
2021-10-13 18:30:46 +0530

    1792

--- leetcode/min_operations.cpp
+++ leetcode/min_operations.cpp
@@ -17,3 +17,25 @@ public:
         return ans;
     }
 };
+
+// another method
+
+vector<int> fun(string boxes){
+    int n = boxes.length();
+    vector<int> ans(n);
+    vector<int> left(n),right(n);
+    int count = boxes[0] - '0';
+    for(int i=1;i<n;i++){
+        left[i] = left[i-1] + count;
+        count += boxes[i] - '0';
+    }
+    count = boxes[n-1] - '0';
+    for(int i=n-2;i>=0;i--){
+        right[i] = right[i+1] + count;
+        count += boxes[i] - '0';
+    }
+    for(int i=0;i<n;i++){
+        ans[i] = left[i] + right[i];
+    }
+    return ans;
+}
