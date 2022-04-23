3e972f3eaafa7a65f1b78f70334a8ceb2ef66bc2
B S Anandu <59107214+BSanandu88@users.noreply.github.com>
2022-04-23 16:00:22 +0530

    Time: 7 ms (39.83%), Space: 7.6 MB (5.80%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
-
+     unordered_map<string, string> codeDB, urlDB;
+    const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     // Encodes a URL to a shortened URL.
+    
+    string getCode(){
+        string code = "";
+        for(int i = 0; i < 6; i++) code += chars[rand()%62];
+         return "http://tinyurl.com/" + code;
+    }
+    
     string encode(string longUrl) {
-        return longUrl;
+        if (urlDB.find(longUrl) != urlDB.end()) return urlDB[longUrl];
+        string code = getCode();
+        while (codeDB.find(code) != codeDB.end()) code = getCode();
+        codeDB[code] = longUrl;
+        urlDB[longUrl] = code;
+        return code;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+         return codeDB[shortUrl];
     }
 };
 
